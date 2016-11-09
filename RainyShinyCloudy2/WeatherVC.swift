1a4999d82bbe96015564d027618d1e41af4c8034
Joy Umali <joyumali@Joys-MacBook-Pro.local>
2016-11-08 17:33:05 -0800

    added files

---
+++ RainyShinyCloudy2/WeatherVC.swift
@@ -0,0 +1,54 @@
+//
+//  WeatherVC.swift
+//  RainyShinyCloudy2
+//
+//  Created by Joy Umali on 11/8/16.
+//  Copyright © 2016 Joy Umali. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var currentTempLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+    }
+    
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+
+
+
+}
+
